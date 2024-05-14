d0e707d875811b99dd8bc1e3fdd718d9c1215b75
Ngumi22 <mwaurapngumi@gmail.com>
2024-05-14 07:51:43 +0300

    Server

--- server/app/api/addProduct.tsx
+++ server/app/api/addProduct.tsx
@@ -2,10 +2,10 @@
 
 import { revalidatePath } from "next/cache";
 import mysql from "mysql2/promise";
-import { z } from "zod";
 
+// Function to connect to the database
 async function connectToDatabase() {
-  const sql = await mysql.createPool({
+  return await mysql.createPool({
     host: "127.0.0.1",
     database: "bernzz",
     port: 3306,
@@ -15,7 +15,6 @@ async function connectToDatabase() {
     connectionLimit: 10,
     queueLimit: 0,
   });
-  return sql;
 }
 
 export async function getAllProducts() {
@@ -23,10 +22,7 @@ export async function getAllProducts() {
     const pool = await connectToDatabase();
     const connection = await pool.getConnection();
 
-    // Query to fetch all products
     const [products] = await connection.query("SELECT * FROM products");
-
-    // Release the connection
     connection.release();
 
     return products;
@@ -36,128 +32,283 @@ export async function getAllProducts() {
   }
 }
 
-// SQL query and fetch a product by its ID
-export async function getProductById(productId: number) {
+export async function getAllCategories() {
   try {
     const pool = await connectToDatabase();
     const connection = await pool.getConnection();
 
-    // Execute query to fetch product by ID
-    const [product] = await connection.query(
-      "SELECT * FROM products WHERE productId = ?",
-      [productId]
-    );
-
+    const [categories] = await connection.query("SELECT * FROM categories");
     connection.release();
 
-    // Check if product is found
-    if (Array.isArray(product) && product.length > 0) {
-      return product[0]; // Return the product
-    } else {
-      return null; // Null if product is not found
-    }
+    return categories;
   } catch (error) {
-    console.error("Error fetching product by ID:", error);
+    console.error("Error fetching categories:", error);
     throw error;
   }
 }
 
-export async function addProduct(state: { message: string }, formData: any) {
-  console.log("Form data received:", formData);
+// Function to handle generic database errors
+async function handleDatabaseError(error: any) {
+  console.error("Database error:", error);
+  throw new Error("An unexpected error occurred. Please try again later.");
+}
 
-  // Using parameterized query to prevent SQL injection
+// Function to add a category
+export async function addCategory(categoryName: string) {
+  try {
+    // Check if the category name meets the criteria
+    if (!/^[a-zA-Z]{3,}$/.test(categoryName)) {
+      // If the category name doesn't meet the criteria, return an error message
+      return {
+        message:
+          "Category name must contain at least 3 letters and only letters.",
+      };
+    }
+
+    const pool = await connectToDatabase();
+    const connection = await pool.getConnection();
+
+    // Check if the category already exists
+    const [existingCategory] = await connection.query(
+      "SELECT * FROM categories WHERE name = ?",
+      [categoryName]
+    );
+
+    if (Array.isArray(existingCategory) && existingCategory.length > 0) {
+      // Category already exists, return an error message
+      return { message: `Category "${categoryName}" already exists.` };
+    } else {
+      // Category does not exist, proceed with insertion
+      const sql = "INSERT INTO categories (name) VALUES (?)";
+      await connection.query(sql, [categoryName]);
+      connection.release();
+
+      return { message: `Category "${categoryName}" added successfully.` };
+    }
+  } catch (error) {
+    await handleDatabaseError(error);
+  }
+}
+
+// Function to add a product
+export async function addProduct(state: { message: string }, formData: any) {
   const sqlInsert =
-    "INSERT INTO products (productName, price, quantity, image) VALUES (?, ?, 1, ?)";
-  const sqlUpdate =
-    "UPDATE products SET quantity = quantity + 1 WHERE productName = ? AND price = ? AND image = ?";
+    "INSERT INTO products (sku, name, description, product_status_id, price, discount_percentage, category_id, quantity, taxable, CPU, RAM, Storage, Ports, Webcam, Connectivity, Processor, OperatingSystem, Weight, ScreenSize, CameraResolution, BatteryLife, PrintSpeed, WiFi, Copying, Scanning, PaperHandling, Consumables, PrinterSoftware, NetworkProtocol, Interface, NetworkCompatibility, SIMCardSlot, WirelessConnectivity, MaxDevicesConnected, Battery, Security, Display, AccessControl, Compatibility, ViewableImageArea, AspectRatio, Contrast, Resolution, Cores, ProcessorFrequency, Memory, Graphics, PowerSupply, Dimensions, created_at, updated_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  const sqlUpdate = "UPDATE products SET quantity = quantity + 1 WHERE sku =?";
+
+  const imageFields = [
+    "main_image",
+    "thumbnail1",
+    "thumbnail2",
+    "thumbnail3",
+    "thumbnail4",
+    "thumbnail5",
+  ];
+  let imagePaths = [];
 
   try {
     const pool = await connectToDatabase();
     const connection = await pool.getConnection();
 
-    // Validates user input to prevent SQL injection
-    const productName = formData.productName;
-    const price = parseFloat(formData.price);
-    const image = formData.image;
+    const {
+      sku,
+      name,
+      description,
+      product_status_id,
+      price,
+      discount_percentage,
+      category_id,
+      quantity,
+      taxable,
+      CPU,
+      RAM,
+      Storage,
+      Ports,
+      Webcam,
+      Connectivity,
+      Processor,
+      OperatingSystem,
+      Weight,
+      ScreenSize,
+      CameraResolution,
+      BatteryLife,
+      PrintSpeed,
+      WiFi,
+      Copying,
+      Scanning,
+      PaperHandling,
+      Consumables,
+      PrinterSoftware,
+      NetworkProtocol,
+      Interface,
+      NetworkCompatibility,
+      SIMCardSlot,
+      WirelessConnectivity,
+      MaxDevicesConnected,
+      Battery,
+      Security,
+      Display,
+      AccessControl,
+      Compatibility,
+      ViewableImageArea,
+      AspectRatio,
+      Contrast,
+      Resolution,
+      Cores,
+      ProcessorFrequency,
+      Memory,
+      Graphics,
+      PowerSupply,
+      Dimensions,
+    } = formData;
 
-    // Validates price input format
-    const priceRegex = /^\d+(\.\d+)?$/;
-    if (!priceRegex.test(formData.price)) {
-      return { message: "Invalid price format. Please enter a valid price." };
+    for (let field of imageFields) {
+      const image = formData[field];
+      if (image) {
+        const imageType = field === "main_image" ? "main" : "thumbnail";
+        const imageData = image[0].buffer;
+        // Cast the result to any to bypass TypeScript's type checking temporarily
+        const [result] = (await connection.query(
+          "INSERT INTO product_images (product_sku, image_type, image_data) VALUES (?,?,?)",
+          [sku, imageType, imageData]
+        )) as any;
+        // Access the insertId from the ResultSetHeader
+        const insertId = result[0].insertId;
+        imagePaths.push({ [imageType]: insertId });
+      }
     }
 
-    // Validates product name
-    const productNameRegex = /^[A-Za-z]+(?:\s+[A-Za-z0-9]+)*$/;
-    if (!productNameRegex.test(productName)) {
-      return {
-        message:
-          "Invalid product name. Product name should only contain letters and spaces.",
-      };
-    }
-
-    // Validates maximum length of product name
-    const maxProductNameLength = 100; // Adjust as needed 100 characters max
-    if (productName.length > maxProductNameLength) {
-      return {
-        message: `Product name cannot exceed the maximum length of ${maxProductNameLength} characters.`,
-      };
-    }
-
-    const sqlInjectionRegex = /['";]|--|\/\*/;
-    if (sqlInjectionRegex.test(productName)) {
-      return { message: "Invalid input. Failed to create product." };
-    }
-    // Check if product with same name and price exists
     const [existingProduct] = await connection.query(
-      "SELECT * FROM products WHERE productName = ? AND price = ? AND image = ?",
-      [productName, price, image]
+      "SELECT * FROM products WHERE sku =?",
+      [sku]
     );
 
     if (Array.isArray(existingProduct) && existingProduct.length > 0) {
-      // If product with same name and price exists, update its quantity
-      await connection.query(sqlUpdate, [productName, price, image]);
-
+      await connection.query(sqlUpdate, [sku]);
       revalidatePath("/");
       return {
-        message: `Product ${productName} already exists. Quantity updated.`,
+        message: `Product with SKU ${sku} already exists. Quantity updated.`,
       };
     } else {
-      // If no matching product exists, insert the new product into the database
-      await connection.query(sqlInsert, [productName, price, image]);
-
+      await connection.query(sqlInsert, [
+        sku,
+        name,
+        description,
+        product_status_id,
+        price,
+        discount_percentage,
+        category_id,
+        quantity,
+        taxable,
+        CPU,
+        RAM,
+        Storage,
+        Ports,
+        Webcam,
+        Connectivity,
+        Processor,
+        OperatingSystem,
+        Weight,
+        ScreenSize,
+        CameraResolution,
+        BatteryLife,
+        PrintSpeed,
+        WiFi,
+        Copying,
+        Scanning,
+        PaperHandling,
+        Consumables,
+        PrinterSoftware,
+        NetworkProtocol,
+        Interface,
+        NetworkCompatibility,
+        SIMCardSlot,
+        WirelessConnectivity,
+        MaxDevicesConnected,
+        Battery,
+        Security,
+        Display,
+        AccessControl,
+        Compatibility,
+        ViewableImageArea,
+        AspectRatio,
+        Contrast,
+        Resolution,
+        Cores,
+        ProcessorFrequency,
+        Memory,
+        Graphics,
+        PowerSupply,
+        Dimensions,
+        new Date(),
+        new Date(),
+      ]);
       revalidatePath("/");
-      return { message: `Product ${productName} added successfully.` };
+      return { message: `Product with SKU ${sku} added successfully.` };
     }
   } catch (error) {
-    console.error("Error adding product:", error);
-    return { message: "Failed to add product. Please try again later." };
+    await handleDatabaseError(error);
   }
 }
 
-// Function to delete a product from the database
+// Function to associate a product with a category
+export async function associateProductWithCategory(
+  productId: number,
+  categoryId: number
+) {
+  try {
+    const pool = await connectToDatabase();
+    const connection = await pool.getConnection();
+
+    const sql =
+      "INSERT INTO product_categories (product_id, category_id) VALUES (?,?)";
+    await connection.query(sql, [productId, categoryId]);
+    connection.release();
+
+    return {
+      message: `Product with ID ${productId} associated with category ID ${categoryId} successfully.`,
+    };
+  } catch (error) {
+    await handleDatabaseError(error);
+  }
+}
+
+// Function to add a tag to a product
+export async function addTagToProduct(productId: number, tagName: string) {
+  try {
+    const pool = await connectToDatabase();
+    const connection = await pool.getConnection();
+
+    const sql = "INSERT INTO product_tags (product_id, tag_name) VALUES (?,?)";
+    await connection.query(sql, [productId, tagName]);
+    connection.release();
+
+    return {
+      message: `Tag "${tagName}" added to product ID ${productId} successfully.`,
+    };
+  } catch (error) {
+    await handleDatabaseError(error);
+  }
+}
+
+// Function to delete a product
 export async function deleteProduct(productId: number) {
   try {
     const pool = await connectToDatabase();
     const connection = await pool.getConnection();
 
-    // Delete the product from the database using the default "id" column
-    const sql = "DELETE FROM products WHERE productId = ?";
+    const sql = "DELETE FROM products WHERE productId =?";
     await connection.query(sql, [productId]);
-
-    // Release the connection and revalidate the cache
     connection.release();
     revalidatePath("/");
-
-    console.log(`Product with ID ${productId} deleted successfully.`); // Log success message
-
+    console.log(`Product with ID ${productId} deleted successfully.`);
     return { message: `Product with ID ${productId} deleted successfully.` };
   } catch (error) {
-    console.error("Error deleting product:", error); // Log error message
-    return { message: "Failed to delete product. Please try again later." };
+    await handleDatabaseError(error);
   }
 }
 
-// Function to edit an existing product in the database
+// Function to edit a product
 export async function editProduct(
   productId: number,
   updatedData: Record<string, any>
@@ -166,21 +317,15 @@ export async function editProduct(
     const pool = await connectToDatabase();
     const connection = await pool.getConnection();
 
-    // Validate and extract updated product data
     const { productName, price, quantity } = updatedData;
 
-    // Update the product in the database using the product ID
     const query =
-      "UPDATE products SET productName = ?, price = ?, quantity = ? WHERE productId = ?";
+      "UPDATE products SET productName =?, price =?, quantity =? WHERE productId =?";
     await connection.query(query, [productName, price, quantity, productId]);
-
-    // Release the connection and revalidate the cache
     connection.release();
     revalidatePath("/");
-
     return { message: `Product with ID ${productId} updated successfully.` };
   } catch (error) {
-    console.error("Error editing product:", error);
-    return { message: "Failed to edit product. Please try again later." };
+    await handleDatabaseError(error);
   }
 }
